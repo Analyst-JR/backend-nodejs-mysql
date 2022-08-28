3da06848d81f812bf5c08e9be14eb02e3187821c
Lucas Eloi de Oliveira <lucase@positivo.com.br>
2022-08-28 07:49:44 -0300

    basic

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const routes = express.Router();
-const UserController = require('./controllers/UserController')
+const UserController = require('./controllers/UserController');
+const AddressController = require('./controllers/AdressController');
 
 routes.get('/users', UserController.index);
-routes.post('/users', UserController.store)
+routes.post('/users', UserController.store);
+
+routes.get('/users/:user_id/addresses', AddressController.index);
+routes.post('/users/:user_id/addresses', AddressController.store)
 
 module.exports = routes;
