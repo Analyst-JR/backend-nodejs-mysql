7f024f45cf71f33b60c93725ad6e22bd92807685
Lucas Eloi de Oliveira <lucase@positivo.com.br>
2022-08-28 14:08:26 -0300

    commit

---
+++ src/controllers/TechController.js
@@ -0,0 +1,10 @@
+const User = require('../model/Tech');
+
+module.exports = {
+    async index(req, res) {
+        
+    },
+    async store(req, res) {
+
+    }
+};
--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const routes = express.Router();
 const UserController = require('./controllers/UserController');
 const AddressController = require('./controllers/AdressController');
+const TechController = require('./controllers/TechController');
 
 routes.get('/users', UserController.index);
 routes.post('/users', UserController.store);
 
 routes.get('/users/:user_id/addresses', AddressController.index);
-routes.post('/users/:user_id/addresses', AddressController.store)
+routes.post('/users/:user_id/addresses', AddressController.store);
+
+routes.get('/users/:user_id/techs', TechController.index);
+routes.post('/users/:user_id/techs', TechController.store)
 
 module.exports = routes;
